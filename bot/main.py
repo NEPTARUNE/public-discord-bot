325496466421306b085c60e738323b070c65fc21
NEPTARUNE <98255890+NEPTARUNE@users.noreply.github.com>
2022-12-07 10:51:28 +0000

    テスト記述1

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,24 @@
 import discord
 import config
+from pyshorteners import Shortener
 
 bot = discord.Bot()
+short = Shortener()
+
+cogs_list = [
+    'hello',
+]
+
+for cog in cogs_list:
+    bot.load_extension(f'cogs.{cog}')
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 @bot.slash_command(guild_ids=[1006776654934323260])
-async def hello(ctx):
+async def url(ctx):
+
     await ctx.respond("Hello!")
 
 bot.run(config.TEST_TOKEN)
